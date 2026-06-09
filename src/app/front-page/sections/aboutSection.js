de719f12328dd53ad273ec7ae8200b1ebbe37c6c
Sarvesh Shivashankaraiah <pixelsaints@users.noreply.github.com>
2026-06-09 11:02:22 +0530

    font changes

--- src/app/front-page/sections/aboutSection.js
+++ src/app/front-page/sections/aboutSection.js
@@ -184,7 +184,7 @@ export default function AboutSection() {
         <div className="container relative">
           <div className="flex flex-col w-full lg:w-[90%] justify-center items-center text-center mx-auto">
             <h5 className="subtitle mb-4">About Real Impact</h5>
-            <h2 className="text-scrub font-serif text-[24px] lg:leading-14 lg:text-[32px] text-white mb-8">Real Impact is a global leader in broadcast production, specializing in the planning, execution, and delivery of high-impact live events and broadcast operations. Backed by a multidisciplinary team of industry experts, we combine creative vision, technical excellence, and production expertise, supported by a comprehensive broadcast equipment rental division.</h2>
+            <h2 className="text-scrub text-[24px] lg:leading-14 lg:text-[32px] text-white mb-8">Real Impact is a global leader in broadcast production, specializing in the planning, execution, and delivery of high-impact live events and broadcast operations. Backed by a multidisciplinary team of industry experts, we combine creative vision, technical excellence, and production expertise, supported by a comprehensive broadcast equipment rental division.</h2>
             <p className="text-white/70 text-lg leading-relaxed w-full lg:w-[60%] mx-auto desc mb-8">Real Impact brings together decades of expertise in broadcast engineering, live production, and content execution, creating seamless experiences for sports, entertainment, and global television audiences.</p>
             <TransitionLink href="/about" className="text-pri-400 btn btn-link">
               About Us
