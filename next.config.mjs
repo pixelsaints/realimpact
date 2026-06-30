afc94caa391b43edc595a72fe831471e8b0dec14
Sarvesh Shivashankaraiah <pixelsaints@users.noreply.github.com>
2026-06-30 20:42:00 +0530

    config changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "standalone",
+
   devIndicators: false,
+
   images: {
     remotePatterns: [
       {
